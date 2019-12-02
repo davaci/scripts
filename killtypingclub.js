4e3f6d49ff6142e9a585212dcd5102df873f56b3
davaci <36366790+davaci@users.noreply.github.com>
2019-12-02 14:52:27 -0500

    Update killtypingclub.js

--- killtypingclub.js
+++ killtypingclub.js
@@ -1,3 +1,5 @@
+alert("activating")
 for (var i = 0; i < 10000000; i++) {
     document.getElementById('main').innerHTML += 'mewo';
+    alert("The current index is: "+i+"! How far can you pc handle? All 10000000?")
 }
