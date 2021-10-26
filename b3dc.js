610a8f652b492b1b8a48a151d32b4bbd62f8ffa9
c0repwn3r <36366790+c0repwn3r@users.noreply.github.com>
2021-10-26 10:22:49 -0400

    Update b3dc.js

--- b3dc.js
+++ b3dc.js
@@ -1,3 +1,3 @@
 d=document,a=app,p=a.player;
 p.killOld=p.kill,p.noclip=0,p.killCount=0,p.kill=function(){if(this.noclip){return;}this.killOld(),this.killCount++,console.log(`kc: ${this.killCount}`)};
-d.onkeypress=(e)=>{if(e.keycode==192){if(p.noclip){p.noclip=0}else{p.noclip=1}}}
+d.onkeypress=(e)=>{if(e.keyCode==192){if(p.noclip){p.noclip=0}else{p.noclip=1}}}
